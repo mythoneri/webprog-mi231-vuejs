12176006d926869dc2048c98ab961cdc74e60d92
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2025-01-27 11:55:33 +0000

    added supabase

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://xcdboqdwkycagrzzrfrk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjZGJvcWR3a3ljYWdyenpyZnJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5Njg0MTYsImV4cCI6MjA1MzU0NDQxNn0.ARZH1dit0FA8msrtOY4vDDPe5SIJvtRVgvV4k50VmMI')
