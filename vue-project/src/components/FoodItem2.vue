34f3fcf8e5619edab1860a2f59a1e12dec6aaaa6
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2025-01-29 14:36:26 +0000

    clean

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -17,10 +17,11 @@ export default {
 </script>
 
 <style>
-.mangoes{
+.mangoes {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
     background-color: lightyellow;
-}</style>   
+    }
+</style>   
