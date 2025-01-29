b7870b2d0ee9789e868740bc10963d25c8ec22e8
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2025-01-29 22:08:05 +0800

    my link

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ewcudpstxexadhxetkyl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Y3VkcHN0eGV4YWRoeGV0a3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyOTUsImV4cCI6MjA1MzU0NDI5NX0.2QFugY3ZCiw_Pll_z2h9nTCMI0VfcF5qh90Jw7n_szA')
+export const supabase = createClient('https://xcdboqdwkycagrzzrfrk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjZGJvcWR3a3ljYWdyenpyZnJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5Njg0MTYsImV4cCI6MjA1MzU0NDQxNn0.ARZH1dit0FA8msrtOY4vDDPe5SIJvtRVgvV4k50VmMI')
