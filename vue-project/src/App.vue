da5f16688272be006aca0e362dacf805aaa1b245
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2025-02-04 11:13:08 +0800

    another testing

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,42 +1,52 @@
 <template>
   <h1>Food</h1>
-     <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-    <br>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
-  <personal-profile/> 
-  <comment-form/>
-  <comment/>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 
+  <br>
+
+  <food-item />
+  <food-item2 />
+  <food-item />
+  <personal-profile />
+  <comment-form />
+  <comment />
+
+  <ul>
+    <li v-for="comment in comments" :key="comment.id">{{ comment.text }}</li>
+  </ul>
 </template>
 
- <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-  const countries = ref([])
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
+const countries = ref([])
+const comments = ref([])
+
+async function getCountries() {
+  const { data, error } = await supabase.from('countries').select()
+  if (!error && data) {
     countries.value = data
   }
+}
 
-  onMounted(() => {
-    getCountries()
-  })
-
-  const comments = ref([])
-
-  async function getComments() {
-    const { data } = await supabase.from('comments').select()
+async function getComments() {
+  const { data, error } = await supabase.from('comments').select()
+  if (!error && data) {
     comments.value = data
   }
+}
 
-  onMounted(() => {
-    getComments()
-  })
-  </script>
-  
+onMounted(() => {
+  getCountries()
+  getComments()
+})
+</script>
